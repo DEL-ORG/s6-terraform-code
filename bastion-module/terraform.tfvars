112187ac0eb4dd7bde0afd5ee32887aef670c7d9
phase5 <phase5@gmail.com>
2024-03-08 05:33:47 +0200

    update

---
+++ bastion-module/terraform.tfvars
@@ -0,0 +1,23 @@
+region = "us-east-1"
+instance_type   = "t2.medium"
+volume_size     = "30"
+pub_ip          = true
+api_termination = true
+cycle = false
+tenancy = "default"
+key = ""
+
+
+tags = {
+  "id"            = "1300"
+  "Teams"         = "phase5"
+  "environment"   = "dev"
+  "project"       = "revive"
+  "createBy"      = "Terraform"
+  "cloudProvider" = "aws"
+  "resource"      = "jenkins-master"
+}
+
+
+
+
--- jenkins-master-module/sg_data.tf
+++ jenkins-master-module/sg_data.tf
@@ -1,7 +1,21 @@
 data "aws_security_group" "revive_sg" {
   filter {
     name   = "tag:Name"
-    values = ["revive-dev-sg"]
+    values = ["jenkins-revive-dev-sg"]
+  }
+  tags = {
+    "id"            = "1300"
+    "Teams"         = "phase5"
+    "environment"   = "dev"
+    "project"       = "revive"
+    "createBy"      = "Terraform"
+  }
+}
+
+data "aws_security_group" "lb_sg" {
+  filter {
+    name   = "tag:Name"
+    values = ["lb-revive-dev-sg"]
   }
   tags = {
     "id"            = "1300"
